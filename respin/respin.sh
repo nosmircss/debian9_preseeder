9afbea044921475a17633fe55a20a982ee58fa7b
Chris Dudek <chris@nwcd.net>
2019-04-18 19:41:38 -0500

    placeholder file

---
+++ respin/respin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 
+# this will check that you have the dependencies installed
+# will take the preseed from ../preseed/preseed.cfg and add it to the iso you supply
+# change the boot menu to instantly boot the auto install
